cd8afcc63b8545c925f9df48f8c538094a4a2419
neel <neelrai906@gmail.com>
2022-02-28 01:56:51 +0530

    2

--- Automation/hk.js
+++ Automation/hk.js
@@ -1,6 +1,7 @@
 const loginLink = "https://www.hackerrank.com/auth/login";
 const puppeteer = require("puppeteer");
 
+const codeFile = require("./code");
 console.log("Before");
 
 let email = "dajefo8967@shackvine.com";
@@ -45,6 +46,78 @@ browserWillLaunchPromise
       { delay: 50 }
     );
     return loginButtonClickPromise;
+  })
+  .then(function () {
+    let algoSecClickedPromise = waitAndClick(
+      'a[data-attr1="algorithms"]',
+      page
+    );
+    return algoSecClickedPromise;
+  })
+  .then(function () {
+    let warmUpSectionClickedPromise = waitAndClick(
+      'input[value="warmup"]',
+      page
+    );
+    return warmUpSectionClickedPromise;
+  })
+  .then(function () {
+    let allQuestionsArrPromise = page.$$(
+      ".ui-btn.ui-btn-normal.primary-cta.ui-btn-line-primary.ui-btn-styled"
+    );
+    return allQuestionsArrPromise;
+  })
+  .then(function (TotalQuestions) {
+    console.log("Number of Questions -> " + TotalQuestions.length);
+    let questionWillBeSolved = questionSolver(
+      page,
+      TotalQuestions[0],
+      codeFile.answers[0]
+    );
+    return questionWillBeSolved;
   });
+
+function waitAndClick(selector, cPage) {
+  return new Promise(function (resolve, reject) {
+    let waitForModalPromise = cPage.waitForSelector(selector);
+    waitForModalPromise
+      .then(function () {
+        let clickModal = cPage.click(selector, { delay: 100 });
+        return clickModal;
+      })
+      .then(function () {
+        resolve();
+      })
+      .catch(function () {
+        reject();
+      });
+  });
+}
+function questionSolver(page, question, answer) {
+  return new Promise(function (resolve, reject) {
+    let questionWillbeClickedPromise = question.click();
+    questionWillbeClickedPromise
+      .then(function () {
+        let waitForEditor = waitAndClick(
+          ".monaco-editor.no-user-select.vs",
+          page
+        );
+        return waitForEditor;
+      })
+      .then(function () {
+        let customInputClicked = waitAndClick(".checkbox-input", page);
+        return customInputClicked;
+      })
+      .then(function () {
+        return waitAndClick(".input.text-area.custominput.auto-width", page);
+      })
+      .then(function () {
+        return page.type(".input.text-area.custominput.auto-width", answer, {
+          delay: 20,
+        });
+      })
+      .then(function () {})
+  })
+}
 
 console.log("After");
